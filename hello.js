248826a638315eb0a3892fc1b6b68710220894e0
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-05 18:39:57 -0700

    file updated

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 console.log('Hello from Node.js');//this Hello will show in consol of the file
 
-import { writeFileSync } from 'fs';//build in function of js 
+const fs = require('fs');//build in function of js 
 
 writeFileSync('hello.txt' , 'hello.js');//it will create a new text file name as hello in this.

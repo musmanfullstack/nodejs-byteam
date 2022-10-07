4e6551f640b9810f82bc53fd797c99d8aa9acd2c
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-06 18:26:47 -0700

    Responce from server file is added in it

---
+++ method-server-responce.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    console.log(req.url, req.method, req.header);
+    res.setHeader('Content-Type' , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>my first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</html>');
+    res.end('');
+});
+//when you run the command you can see that their is alot of changing in it. In the browser you can see the html written code on your running server.
+server.listen(3000);

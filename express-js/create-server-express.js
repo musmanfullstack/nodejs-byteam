cc493d8d76190533b51b076a906fb9fe79afcd67
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-12 23:29:36 -0700

    Creating server in express

---
+++ express-js/create-server-express.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+
+const app = express ();
+
+app.use((req, res, next)=>{
+    console.log('in the middleware!');
+    next();
+});
+app.use((req, res, next)=>{
+    console.log('in another middleware!');
+    res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3000);//same working we are doing just using singal function listen init.

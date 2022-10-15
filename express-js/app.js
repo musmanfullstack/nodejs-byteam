b2fa3568c8ea3619b1c07cb855caefce10bd7baf
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-15 12:51:21 -0700

    404 error page enter

--- express-js/app.js
+++ express-js/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,6 +14,6 @@ app.use(shopRoutes);
 app.use('/admin',adminRoutes);   //in this we declare that in every link belongs to admin have to show the admin before every link.
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 app.listen(3000);

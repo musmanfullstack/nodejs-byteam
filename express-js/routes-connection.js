5be9238bf350ea976b87421e2c84d65a89990cf5
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-14 19:00:18 -0700

    404 page added

--- express-js/routes-connection.js
+++ express-js/routes-connection.js
@@ -11,4 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(shopRoutes);    //declare the contant paths
 app.use(adminRoutes);   //declare the constant paths
 
+app.use((req, res, next)=>{//for show page not found error if any other url is enter.
+    res.status(404).send('<h1>Page not found</h1>');
+});
 app.listen(3000);

87174f1fd5aeede6ecad9308246a1940ffa9f87b
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-17 14:11:20 -0700

    Modify the files

--- express-js/app2.js
+++ express-js/app2.js
@@ -1,8 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
 const adminRoutes = require('./routes-by-express/admin3');
 const shopRoutes = require('./routes-by-express/shop3');
 
@@ -10,7 +14,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(shopRoutes);    
 app.use('/admin',adminRoutes);   //in this we declare that in every link belongs to admin have to show the admin before every link.
-
 
 app.use((req, res, next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
---
+++ express-js/routes-by-express/shop4.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const router = express.Router();//declaring routers
+
+router.use('/add-student',(req, res, next)=>{
+    res.render('shop');
+    //res.sendFile(path.join(__dirname,'../' ,'views', 'shop.html'));//the dirname is a global variable which holds absolue path in the variable. In it path join didect your operating system and make the path. when it goes in dir name it takes the location of this file then ../ takes it one step up means out of the folder and then in views folder and shop file.
+});
+
+module.exports = router;

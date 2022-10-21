2971e7464e0f12ccc3bfb330975b7ba802eec3ca
Rana Husnain <RanaHusnainArif162@gmail.com>
2022-10-20 18:17:46 -0700

    MOdify file of html

---
+++ express-js/app3.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+const adminData = require('./routes-by-express/admin5');
+const shopData = require('./routes-by-express/shop5');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(shopData.routes);    
+app.use('/admin',adminData.routes);   //in this we declare that in every link belongs to admin have to show the admin before every link.
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+app.listen(3000);
